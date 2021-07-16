cd8fcd89d7b4f0e263c9970d5869ea8fa051e0a6
Martin Lui <mrtilui@amazon.com>
2021-07-15 17:50:44 -0700

    Fix reportrepair

---
+++ ReportRepair/src/main/kotlin/ReportRepair.kt
@@ -0,0 +1,64 @@
+import java.io.File
+import java.lang.IllegalArgumentException
+
+class ReportRepair {
+    val YEAR: Int = 2020
+
+    // Assumptions
+    //  There will always be at least one pair of entries summing to 2020 (otherwise exception)
+    //  If multiple, return the first found going left-to-right
+    //  Negative and zero are allowed
+    fun repair(numbers: List<Int>): Int {
+        val p = twoSum(numbers, YEAR)
+        if (p != null) {
+            return p.first * p.second
+        }
+        throw IllegalArgumentException("List does not have pair that sums to $YEAR")
+    }
+
+    // Given a list of numbers, returns two numbers in the list that sum to target
+    // Returns null if no matching pair
+    fun twoSum(numbers: List<Int>, target: Int): Pair<Int, Int>? {
+        val (ans, _) = numbers.fold((null as Int? to mutableSetOf<Int>())){ (ans, seen), element ->
+            when {
+                ans != null -> (ans to seen)
+                seen.contains(target-element) -> (element to seen)
+                else -> {
+                    seen.add(element)
+                    (ans to seen)
+                }
+            }
+        }
+        if (ans != null) {
+            return ans to (target-ans)
+        }
+        return null
+    }
+
+
+    fun repair2(numbers: List<Int>): Int {
+        val t = threeSum(numbers, YEAR)
+        if (t != null) {
+            return t[0] * t[1] * t[2]
+        }
+        throw IllegalArgumentException("List does not have triple that sums to $YEAR")
+    }
+
+    fun threeSum(numbers: List<Int>, target: Int) : List<Int>?{
+        val ans = numbers.foldIndexed(null as List<Int>?){idx, ans, element ->
+            val twoSumAns = twoSum(numbers.drop(idx+1), target-numbers[idx])
+            when {
+                ans != null || twoSumAns == null -> ans
+                else -> listOf(numbers[idx], twoSumAns.first, twoSumAns.second)
+            }
+        }
+        return ans ?: null
+    }
+}
+
+fun main() {
+    val input: List<String> = File("input.txt").readLines()
+    val reportRepair = ReportRepair()
+    println(reportRepair.repair(input.map{it.toInt()}))
+    println(reportRepair.repair2(input.map{it.toInt()}))
+}
---
+++ ReportRepair/src/test/kotlin/ReportRepairTest.kt
@@ -0,0 +1,77 @@
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import java.lang.IllegalArgumentException
+
+class ReportRepairTest: DescribeSpec({
+    val reportRepair = ReportRepair()
+    
+    describe("reportRepair") {
+        context("When given no numbers") {
+            it("throws exception") {
+                shouldThrow<IllegalArgumentException> {
+                    reportRepair.repair(listOf())
+                }
+            }
+        }
+        context("Answer is two of the same number") {
+            it("returns product") {
+                val actual = reportRepair.repair(listOf(1010,1010))
+                actual shouldBe 1020100
+            }
+        }
+        context("Should not return element matched with itself") {
+            it("throws exception") {
+                shouldThrow<IllegalArgumentException> {
+                    reportRepair.repair(listOf(1010))
+                }
+            }
+        }
+        context("Input has negative values") {
+            it("returns product") {
+                val actual = reportRepair.repair(listOf(-1, -2, -3, 2018, 2019, 2020, 2021))
+                actual shouldBe -2021
+            }
+            it ("returns product") {
+                val actual = reportRepair.repair(listOf(2000, -2000, 20))
+                actual shouldBe 40000
+            }
+        }
+        context("Multiple answers") {
+            it("Returns first") {
+                val actual = reportRepair.repair(listOf(2020, 0, 1000, 1020, 2020, 2019, 1))
+                actual shouldBe 0
+            }
+        }
+    }
+
+    describe("reportRepair2") {
+        context("Empty list") {
+            it("throw exception") {
+                shouldThrow<IllegalArgumentException> {
+                    reportRepair.repair2(listOf())
+                }
+            }
+        }
+        context("List of 0") {
+            it("throw exception") {
+                shouldThrow<IllegalArgumentException> {
+                    reportRepair.repair2(listOf())
+                }
+            }
+        }
+        context("Zero is a factor") {
+            it("returns 0") {
+                val actual = reportRepair.repair2(listOf(1000, 1020, 2, 5, 0))
+                actual shouldBe 0
+            }
+        }
+        context("Multiple of the same entry") {
+            it("returns product") {
+                val actual = reportRepair.repair2(listOf(100, 200, 300, 2, 2, 2016, 3000))
+                actual shouldBe 8064
+            }
+        }
+    }
+
+})
